806feb746be0e101913bbc1f7492b60990ff2285
Karl911 <36680178+Karl911@users.noreply.github.com>
2022-09-02 17:53:29 +0200

    implements edit dialog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -43,6 +43,13 @@ export class AppComponent implements OnInit{
       }
     })
   }
+
+  editProduct(row : any) {
+    this.dialog.open(DialogComponent, {
+      width : '30%',
+      data : row 
+    })
+  }
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
